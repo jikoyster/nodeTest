64bcfc2f8be1795e81cc4f378bb306f6e1d8d1a9
Jake <yu.jake@gmail.com>
2017-07-30 11:46:54 +0800

    nodejstest: using babel-register

---
+++ nodejstest/server/index.babel.js
@@ -0,0 +1,4 @@
+import test from './test';
+
+console.log("babel is still working");
+test("hello here");
---
+++ nodejstest/server/index.js
@@ -0,0 +1,5 @@
+import test from './test';
+
+
+console.log("babel is still working");
+test("hello here");
